6108bd6bfa51ae6695f7062f38e7375333e3ef74
Carlos Zuluaga <carlosez9@hotmail.com>
2019-06-19 21:46:50 +0000

    Adding quote file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
