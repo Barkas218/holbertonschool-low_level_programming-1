69ff5bb73e6033d094bfd83701f4f3b16575ed11
Carlos Zuluaga <carlosez9@hotmail.com>
2019-06-19 22:04:41 +0000

    Fixing indendation on 101-quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
  *
  * Return: Always 0 (Success)
  */
+
 #include <stdio.h>
 #include <<unistd.h>
 
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-        return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
 }
