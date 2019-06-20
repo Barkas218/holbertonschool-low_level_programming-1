79e724eadebb54ae37a775d48ae86ade530e630e
Carlos Zuluaga <carlosez9@hotmail.com>
2019-06-20 19:58:28 +0000

    Adding exercise 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,17 +14,22 @@ int main(void)
 
 	int i;
 	int j;
+	int k;
 
 	for (i = 48; i <= 57; i++)
 	{
 		for (j = i + 1; j <= 57; j++)
 		{
-			putchar(i);
-			putchar(j);
-			if (i < 56 || j < 57)
+			for (k = j + 1; k <= 57; k++)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i < 55 || j < 56 || k < 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
