2fbfd1566e98dcbe996e90a690c64df71aaf6442
Carlos Zuluaga <carlosez9@hotmail.com>
2019-06-25 19:36:19 +0000

    Fixing exercise 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,33 +1,31 @@
 #include "holberton.h"
 #include <stdio.h>
-
 /**
  * print_to_98 - Print the numbers from the input to 98..
  * @n: Holds the integer for the function.
  *
  * Return: Always 0.
  */
-
 void print_to_98(int n)
 {
-        int i = n;
+	int i = n;
 
-        while (1)
-        {
-                if (i < 98)
-                {
-                        i++;
-                        printf("%d, ", i);
-                }
-                else if (i == 98)
-                {
-                        printf("%d\n", i);
-                        break;
-                }
-                else
-                {
-                        i--;
-                        printf("%d, ", i);
-                }
-        }
+	if (i <= 98)
+	{
+		for (i = 0; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (i = 0; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+				printf(", ");
+		}
+	}
 }
