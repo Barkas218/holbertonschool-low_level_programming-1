ddef99200c0837fa76ded54a969da68b1d8cc4ab
Carlos Zuluaga <carlosez9@hotmail.com>
2019-06-25 19:40:09 +0000

    Fixing exercise 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,11 @@
  */
 void print_to_98(int n)
 {
-	int i = n;
+	int i;
 
-	if (i <= 98)
+	if (n <= 98)
 	{
-		for (i = 0; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i < 98)
@@ -21,11 +21,12 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = 0; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
 			if (i > 98)
 				printf(", ");
 		}
 	}
+	printf("\n");
 }
