3e9688c40584d70d829c24fa9bb2bd79571eef4d
Carlos Zuluaga <carlosez9@hotmail.com>
2019-06-26 03:40:54 +0000

    Fixing algorithm exercise 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,85 +1,42 @@
 #include "holberton.h"
 #include <stdio.h>
-
 /**
  * times_table - Prints the 9 times_table.
  *
  * Return: Always 0.
  */
-
 void times_table(void)
 {
-	int i, j, result = 0, fc, sc, tc;
+	int i, j, result = 0, fc, sc;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-
-			if (j < 9)
+			if (j > 0)
 			{
-				if (result < 10)
-				{
-				_putchar('0' + result);
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				}
-				else if (result > 10 && result < 100)
+				if (result >= 10 && result < 100)
 				{
 					fc = result / 10;
 					sc = result % 10;
-					_putchar(fc + '0');
-					_putchar(sc + '0');
-					_putchar(' ');
+					_putchar('0' + fc);
+					_putchar('0' + sc);
 				}
-				else if (result > 100)
-				{
-					fc = result / 100;
-					sc = (result / 10) % 10;
-					tc = result % 10;
-					_putchar(fc + '0');
-					_putchar(sc + '0');
-					_putchar(tc + '0');
-				}
-				else
+				else if (result < 10)
 				{
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
+					_putchar('0' + result);
 				}
 			}
 			else
 			{
-                                _putchar('0' + result);
-                                _putchar(',');
-                                if (result > 10 && result < 100)
-				{
-                                        fc = result / 10;
-                                        sc = result % 10;
-                                        _putchar(' ');
-                                        _putchar(fc + '0');
-                                        _putchar(sc + '0');
-				}
-                                else if (result > 100)
-				{
-                                        fc = result / 100;
-                                        sc = (result / 10) % 10;
-                                        tc = result % 10;
-                                        _putchar(fc + '0');
-                                        _putchar(sc + '0');
-                                        _putchar(tc + '0');
-				}
-                                else
-                                {
-					_putchar(' ');
-                                        _putchar(' ');
-                                        _putchar(result + '0');
-					_putchar('$');
-				}
+				_putchar(result + '0');
 			}
 		}
-		_putchar('\n');
+	_putchar('$');
+	_putchar('\n');
 	}
 }
