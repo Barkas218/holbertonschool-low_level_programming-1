cc4cecba1940badc1299bbef24973d794b2e8091
Carlos Zuluaga <carlosez9@hotmail.com>
2019-06-28 03:23:59 +0000

    Adding exercise 12

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - Return the largest prime factor for 612852475143 .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int i;
+	long int n = 612852475143;
+
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+			n = n - i;
+		i--;
+	}
+	printf("%ld\n", i);
+	return (0);
+}
