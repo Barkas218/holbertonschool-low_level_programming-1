c062fdd6de0a4ea8caf41d1fa712eb1977259b0f
Carlos Zuluaga <carlosez9@hotmail.com>
2019-07-08 17:36:58 +0000

    Adding exercise 0-1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _memset - Function that fills memory with a constant byte.
+ * @s: Pointer to the memory area
+ * @b: Value to replace
+ * @n: Number of bytes
+ *
+ * Return: Pointer to the string modified.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+
+	return (s);
+}
