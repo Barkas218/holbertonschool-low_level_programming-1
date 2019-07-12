9959b7d864f423e8732d819205bc5e32ef16199a
Carlos Zuluaga <carlosez9@hotmail.com>
2019-07-12 23:02:42 +0000

    Adding exercise 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * main - Prints the addition of the arguments
+ * @argc: Number of arguments passed
+ * @argv: Pointer to array of arguments passed
+ *
+ * Return: 0 - Success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j = 0;
+	int add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		while (*(*(argv + i) + j) != '\0')
+		{
+			if (*(*(argv + i) + j) < '0' || *(*(argv + i) + j) > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		j = 0;
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
