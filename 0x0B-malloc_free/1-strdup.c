1929d2c997191f9f88181215ae97e867cf3c7367
Carlos Zuluaga <carlosez9@hotmail.com>
2019-07-16 17:31:45 +0000

    Adding exercise 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Copies a string and returns a pointer to the array.
+ * @str: String to be copied.
+ *
+ * Return: Pointer to the array or NULL.
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *dest;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (*(str + i))
+		i++;
+	i++;
+
+	dest = malloc(sizeof(char) * i);
+
+	for (; j < i; j++)
+		*(dest + j) = *(str + j);
+
+	return (dest);
+}
