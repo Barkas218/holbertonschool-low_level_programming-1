810f5df3b964897a164a0a540fc4069bc88734c7
Carlos Zuluaga <carlosez9@hotmail.com>
2019-07-16 20:45:41 +0000

    Fixing syntax error on exercise 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 		*(p + i) = malloc(sizeof(int) * width);
 		if (*(p + i) == NULL)
 		{
-			for (; i <= 0; i--)
+			for (; i >= 0; i--)
 			{
 				free(*(p + i));
 			}
