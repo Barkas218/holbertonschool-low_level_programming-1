b4bbbca892bed4a222735af3bf7a7a91be67b23a
Carlos Zuluaga <carlosez9@hotmail.com>
2019-07-22 21:25:10 +0000

    Adding exercise 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Creates a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Onwer's name of the dog
+ *
+ * Description: Creates a dog
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG */
