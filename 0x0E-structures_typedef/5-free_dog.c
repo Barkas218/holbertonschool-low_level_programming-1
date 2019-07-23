037c17cb06c404874265ddc2bf33b3d65c2c9194
Carlos Zuluaga <carlosez9@hotmail.com>
2019-07-23 01:46:07 +0000

    Adding exercise 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Function that frees dogs.
+ * @d: Pointer to a struct dog.
+ *
+ * Return: Nothing (void).
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
