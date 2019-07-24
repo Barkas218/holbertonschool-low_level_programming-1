3fb8854af19308d1f422e17a3676e979a8f04656
Carlos Zuluaga <carlosez9@hotmail.com>
2019-07-24 18:45:31 +0000

    Fixing betty style for exercise 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,10 +4,10 @@
 /**
  * int_index - check if a number is equal to 98
  * @array: the integer to check
- * @size:
- * @cmp:
+ * @size: size
+ * @cmp: pointer to function
  *
- * Return: 0 if false, something else otherwise.
+ * Return: Index or -1 if error or not found.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
