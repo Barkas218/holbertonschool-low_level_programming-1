9c9ce5d9b583110284034ec2bb6b511a2676aad1
Carlos Zuluaga <carlosez9@hotmail.com>
2019-07-26 16:29:54 +0000

    Add exercise 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Print numbers with a separator.
+ * @separator: Separator
+ * @n: Quantity of numbers
+ *
+ * Return: Result of adding the numbers or 0 if not arguments were passed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list arguments;
+
+	if (separator == NULL)
+		return;
+
+	va_start(arguments, n);
+
+	for (i = 0; i < (n - 1); i++)
+		printf("%d%c ", va_arg(arguments, int), *separator);
+
+	printf("%d\n", va_arg(arguments, int));
+	va_end(arguments);
+}
