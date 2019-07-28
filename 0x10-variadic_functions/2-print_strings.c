356813fa21c4df2e99446fbe239f5c461d2588b2
Carlos Zuluaga <carlosez9@hotmail.com>
2019-07-28 01:37:37 +0000

    Fix betty style on exercise 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,8 +23,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", p);
+
 		if (separator != NULL && i < (n - 1))
-				printf("%s", separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(arguments);
