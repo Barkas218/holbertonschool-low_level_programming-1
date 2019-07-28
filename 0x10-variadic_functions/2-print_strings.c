20fe4b8014acf04f5ae4c3f652e670de17d5419e
Carlos Zuluaga <carlosez9@hotmail.com>
2019-07-28 01:34:42 +0000

    Fix if statement null string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,12 +20,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		p = va_arg(arguments, char *);
 		if (p == NULL)
-		{
 			printf("(nil)");
-		}
-		printf("%s", p);
+		else
+			printf("%s", p);
 		if (separator != NULL && i < (n - 1))
-			printf("%s", separator);
+				printf("%s", separator);
 	}
 	printf("\n");
 	va_end(arguments);
