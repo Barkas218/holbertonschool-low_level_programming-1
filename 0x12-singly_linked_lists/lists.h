83d3e8b5af02db70000359371124458cea7c7621
Carlos Zuluaga <carlosez9@hotmail.com>
2019-08-01 20:58:59 +0000

    Add exercise 5

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,6 +24,7 @@ size_t print_list(const list_t *);
 size_t list_len(const list_t *);
 list_t *add_node(list_t **, const char *);
 list_t *add_node_end(list_t **, const char *);
+void print_start(void) __attribute__ ((constructor));
 void free_list(list_t *);
 
 #endif /* LISTS */
