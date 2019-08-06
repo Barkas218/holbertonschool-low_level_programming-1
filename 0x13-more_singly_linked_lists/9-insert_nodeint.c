bbdc71af402833378e6854eeea61362c0b7d9473
Carlos Zuluaga <carlosez9@hotmail.com>
2019-08-06 22:10:06 +0000

    Handle negative numbers in exercise9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	listint_t *tmp = *head;
 
-	if (head == NULL)
+	if (head == NULL || idx < 0)
 		return (NULL);
 
 	if (idx == 0)
