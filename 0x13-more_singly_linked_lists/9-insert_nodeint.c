ed44dc9f576c005cc063d26337f2d6b57a295c06
Carlos Zuluaga <carlosez9@hotmail.com>
2019-08-06 14:41:08 +0000

    Correcting memory allocation in exercise 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,8 +21,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (tmp == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(listint_t));
-
 	while (i < (idx - 1))
 	{
 		tmp = tmp->next;
@@ -30,6 +28,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		i++;
 	}
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
 
 	new->n = val;
 	new->next = tmp->next;
