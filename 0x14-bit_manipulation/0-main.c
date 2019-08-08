397de338c417fe339a25a56fcb905cd133936c52
Carlos Zuluaga <carlosez9@hotmail.com>
2019-08-08 18:07:09 +0000

    Fix return if b is null in exercise 0

--- 0x14-bit_manipulation/0-main.c
+++ 0x14-bit_manipulation/0-main.c
@@ -20,5 +20,11 @@ int main(void)
 	printf("%u\n", n);
 	n = binary_to_uint("0000000000000000000110010010");
 	printf("%u\n", n);
+	n = binary_to_uint(NULL);
+	printf("%u\n", n);
+	n = binary_to_uint("1111111111111111111111111111111111111111111111111111111111111111");
+	printf("%u\n", n);
+	n = binary_to_uint("0");
+	printf("%u\n", n);
 	return (0);
 }
