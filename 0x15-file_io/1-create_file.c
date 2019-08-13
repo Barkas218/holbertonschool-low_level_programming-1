dd669ad20e8d52e3ee833f8f7b0485a33b835737
Carlos Zuluaga <carlosez9@hotmail.com>
2019-08-13 18:36:22 +0000

    Trying fix exercise 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,9 +23,6 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	for (len = 0; text_content[len] != '\0'; len++)
-		;
-
 	if (text_content != NULL)
 	{
 
