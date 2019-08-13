4cebc9fc94594ea7f5a0cd48dcffa820ce893e52
Carlos Zuluaga <carlosez9@hotmail.com>
2019-08-13 18:34:33 +0000

    Trying fix exercise 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,7 +36,7 @@ int create_file(const char *filename, char *text_content)
 
 		if (bytes_wrote == -1)
 		{
-			write(STDOUT_FILENO, "fails", 5);
+			write(STDOUT_FILENO, "fails", 6);
 			return (-1);
 		}
 	}
