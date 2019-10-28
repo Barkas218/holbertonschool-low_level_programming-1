8e69dc45af68b140776c9c4d9a4c44be48cadcf0
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-28 15:48:09 -0500

    Adding functions in header file

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -25,9 +25,10 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void quick_sort_lomuto(int *array, int lo, int hi, size_t size);
 int partition_lomuto(int *array, int lo, int hi, size_t size);
-
+void swap(listint_t *left, listint_t *node_2, listint_t **list);
 
 
 
