c2df031ac3323ff71d31c6565c65f283f063d6a2
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-28 10:48:09 -0500

    Adding exercise 2

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -1,4 +1,36 @@
-//
-// Created by carlos on 26/10/19.
-//
+#include "sort.h"
 
+/**
+ * selection_sort - Sorting an array of number using selection sort
+ * @array: Array
+ * @size: Size of the array
+ * Return: Nothing
+**/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, index_lesser;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		index_lesser = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[index_lesser])
+				index_lesser = j;
+		}
+
+		if (index_lesser != i)
+		{
+			tmp = array[i];
+			array[i] = array[index_lesser];
+			array[index_lesser] = tmp;
+			print_array(array, size);
+		}
+	}
+}
